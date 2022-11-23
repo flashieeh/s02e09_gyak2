ddf477d82a5be048c49f01f4a94c3c4820ae3ae7
dju2zk <dju2zk@SZA0030896>
2022-11-23 15:13:13 +0100

    kész a feladat.

---
+++ s02e09_gyak2/wwwroot/geek.js
@@ -0,0 +1,29 @@
+﻿var viccek;
+
+var letöltés = function () {
+    fetch("jokes.json")
+        
+        .then(data => data.json())
+        .then(data => ddl(data));
+}
+
+function ddl(d) {
+    console.log("siker")
+    console.log(d)
+    viccek = d;
+
+    for (var i = 0; i < viccek.length; i++) {
+        var új = document.createElement("div");
+
+        új.innerHTML = viccek[i].text;
+        document.getElementById("ide").appendChild(új);
+
+
+    }
+}
+
+
+
+window.addEventListener("load", (event) => {
+    letöltés();
+});
---
+++ s02e09_gyak2/wwwroot/jsgyak.js
@@ -0,0 +1,31 @@
+﻿var faktoriális = n => {
+    if (n === 0 || n === 1) {
+        return 1;
+    }
+    else {
+        return n * faktoriális(n - 1)
+    }
+}
+
+function hozzáad() {
+    var pascal = document.getElementById("pascal");
+    var meret = 10;
+    for (var s = 0; s <= meret; s++) {
+        var ujSor = document.createElement("div")
+        ujSor.classList.add("sor");
+
+        pascal.appendChild(ujSor);
+        for (var o = 0; o <= s; o++) {
+
+            var ujElem = document.createElement("div")
+            ujElem.innerHTML = `${faktoriális(s) / (faktoriális(o) * (faktoriális(s - o)))}`
+            ujElem.classList.add("elem");
+
+
+            ujSor.appendChild(ujElem);
+        }
+    }
+}
+hozzáad();
+
+
