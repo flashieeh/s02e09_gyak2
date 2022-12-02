5b40ced9ac71b234628fdfc30966c4467c3861f9
Kristóf Pintér <kristof.pinter@stud.uni-corvinus.hu>
2022-12-02 14:22:10 +0100

    #5 elakadtam

--- s02e09_gyak2/wwwroot/main.js
+++ s02e09_gyak2/wwwroot/main.js
@@ -1,26 +1,50 @@
-fetch('/questions/4')
-    .then(response => response.json())
-    .then(data => console.log(data)
-);
+var joValasz;
+var questionID = 4;
 
 function kerdesmegj(kerdes) {
     console.log(kerdes);
-    document.getElementById("kerdes_szöveg").innerText = kerdes.questionText
-    document.getElementById("válasz1").innerText = kerdes.answer1
-    document.getElementById("válasz2").innerText = kerdes.answer2
-    document.getElementById("válasz3").innerText = kerdes.answer3
-    document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kerdes.image;
+    document.getElementById("kerdes_szoveg").innerText = kerdes.questionText
+    document.getElementById("valasz1").innerText = kerdes.answer1
+    document.getElementById("valasz2").innerText = kerdes.answer2
+    document.getElementById("valasz3").innerText = kerdes.answer3
+    joValasz = kerdes.correctAnswer;
+    if (kerdes.image) {
+        document.getElementById("kep").src = "https://szoft1.comeback.hu/hajo/" + kerdes.image;
+        document.getElementById("kep").classList.remove("rejtett")
+    }
+    else {
+        document.getElementById("kep").classList.add("rejtett")
+    }
+    document.getElementById("valasz1").classList.remove("jó", "rossz");
+    document.getElementById("valasz2").classList.remove("jó", "rossz");
+    document.getElementById("valasz3").classList.remove("jó", "rossz");
+
 }
 
-function kerdesBetolt(id) {
+function kerdesbetolt(id) {
     fetch(`/questions/${id}`)
         .then(response => {
             if (!response.ok) {
                 console.error(`Hibás válasz: ${response.status}`)
             }
             else {
-                return response.json()
+                kerdesmegj(response.json())
             }
         })
-        .then(data => kerdesmegj(data));
+}
+
+function elore() {
+    questionID++;
+    kerdesbetolt(questionID);
+}
+function vissza() {
+    questionID--;
+    kerdesbetolt(questionID);
+}
+
+window.onload = function (e) {
+    console.log("oldal betöltve");
+    document.getElementById("elore_gomb").onclick = elore;
+    document.getElementById("vissza_gomb").onclick = vissza;
+    kerdesbetolt(questionID)
 }
