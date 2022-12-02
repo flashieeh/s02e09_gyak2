76864047104a9521b98debd7cb90a69f089cc49d
Kristóf Pintér <kristof.pinter@stud.uni-corvinus.hu>
2022-12-02 13:49:31 +0100

    #2 kész

---
+++ s02e09_gyak2/Controllers/BoatController.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Mvc;
+using s02e09_gyak2.Models;
+
+namespace s02e09_gyak2.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+        public IActionResult AllQuestions()
+        {
+            HajosContext context = new HajosContext();
+            var kérdések = from x in context.Questions select x.Question1;
+
+            return Ok(kérdések);
+        }
+    }
+}
